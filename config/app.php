27fec5e7e755c80aaafda1e3e89d321b069dddd5
Sabinaya Khadka <sabinayakhadka3@gmail.com>
2026-09-19 14:40:31 +0545

    refine

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+     if (!defined('ROOT_PATH')) {
+        define('ROOT_PATH', dirname(__DIR__) . '/');
+     }
+     if (!defined('BASE_URL')) {
+         define('BASE_URL', '/portfolio-management-system/');
+    }
+    ?>
